389b945be632317b9302f36599ac3e146f7f3afd
kiefuh <61067888+kiefuh@users.noreply.github.com>
2020-11-10 16:14:11 -0500

    Removed unused elements

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -3,21 +3,9 @@ package demo;
 import java.util.LinkedList;
 import java.util.Set;
 
-import model.IntersectGraph;
-import util.IntersectGraphUtil;
-
 public class Demo {
 
 	public static void main(String[] args) {
-		IntersectGraph intersectGraph= new IntersectGraph();
-		intersectGraph.addIntersect("0", 10);
-		intersectGraph.addIntersect("1", 30);
-		intersectGraph.addIntersect("2", 100);
-		intersectGraph.addIntersect("3", 20);
-		intersectGraph.addEdge("0", "1");
-		intersectGraph.addEdge("1", "2");
-		intersectGraph.addEdge("0", "3");
-		intersectGraph.addEdge("3", "2");
 		
 		}
 
