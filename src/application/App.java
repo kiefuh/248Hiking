e8f3ee354993b418bf77ead2a7b855a05e5e787c
kiefuh <61067888+kiefuh@users.noreply.github.com>
2020-11-11 22:18:07 -0500

    Added loader and launching to App

--- src/application/App.java
+++ src/application/App.java
@@ -1,19 +1,23 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
 
 	}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void start(Stage primaryStage) throws Exception {
+		Parent root= FXMLLoader.load(getClass().getResource("Login.fxml"));
+		primaryStage.setScene(new Scene(root));
+		primaryStage.show();
 	}
 
 }
