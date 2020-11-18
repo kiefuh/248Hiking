6869f38b7ff336d44cd89f3b1d8c9f6b3473a6ba
kiefuh <61067888+kiefuh@users.noreply.github.com>
2020-11-17 22:53:23 -0500

    updated selection holder

--- src/util/SelectionHolder.java
+++ src/util/SelectionHolder.java
@@ -1,12 +1,22 @@
 package util;
 
 import model.HikingHistory;
+import model.Trail;
 import model.UserProfile;
 
 public class SelectionHolder {
 	private static HikingHistory selected;
 	private static UserProfile selectedUser;
+	private static Trail selectedTrail;
 	
+	public static Trail getSelectedTrail() {
+		return selectedTrail;
+	}
+
+	public static void setSelectedTrail(Trail selectedTrail) {
+		SelectionHolder.selectedTrail = selectedTrail;
+	}
+
 	public static void setSelected(HikingHistory select) {
 		selected=select;
 	}
